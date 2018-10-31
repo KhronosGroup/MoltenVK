403cedf0c838d18e64bee6d44691e9910179205c
Bill Hollings <bill.hollings@brenwill.com>
2018-10-31 00:18:25 -0400

    Add README placeholder files for empty template directories.

--- MoltenVK/scripts/create_framework.sh
+++ MoltenVK/scripts/create_framework.sh
@@ -10,3 +10,4 @@ rm -rf "${MVK_BUILT_FRWK_PATH}"
 cp -a  "${MVK_TMPLT_PATH}/${PRODUCT_NAME}.framework" "${BUILT_PRODUCTS_DIR}"
 cp -a  "${BUILT_PRODUCTS_DIR}/lib${PRODUCT_NAME}.a" "${MVK_BUILT_FRWK_CONTENT_PATH}/${PRODUCT_NAME}"
 cp -a "${PROJECT_DIR}/include/${PRODUCT_NAME}/" "${MVK_BUILT_FRWK_CONTENT_PATH}/Headers"
+rm -f "${MVK_BUILT_FRWK_CONTENT_PATH}/Headers/README"	#Remove git empty directory placeholder file
