d367bc710921baa3540bf6a33375d9c3e49086cf
Ryan Schmidt <git@ryandesign.com>
2018-11-05 06:35:44 -0600

    Use clang++ not clang to link C++ code
    When using a C++ compiler it's unnecessary to specify -lc++ because
    that's done automatically.

--- MoltenVK/scripts/create_dylib.sh
+++ MoltenVK/scripts/create_dylib.sh
@@ -10,7 +10,7 @@ if test x"${ENABLE_THREAD_SANITIZER}" = xYES; then
 	MVK_TSAN="-fsanitize=thread"
 fi
 
-clang \
+clang++ \
 -dynamiclib ${MVK_TSAN} \
 -arch ${MVK_ARCH} \
 -m${MVK_OS}-version-min=${MVK_MIN_OS_VERSION} \
@@ -21,6 +21,6 @@ clang \
 -iframework ${MVK_SYS_FWK_DIR}  \
 -framework Metal ${MVK_IOSURFACE_FWK} -framework ${MVK_UX_FWK} -framework QuartzCore -framework IOKit -framework Foundation \
 --library-directory ${MVK_USR_LIB_DIR} \
--lSystem  -lc++ \
+-lSystem \
 -o "${BUILT_PRODUCTS_DIR}/${MVK_DYLIB_NAME}" \
 -force_load "${BUILT_PRODUCTS_DIR}/lib${PRODUCT_NAME}.a"
