5c8a472a1b683afd92918029ffe6d8d2d02c31d1
Chip Davis <cdavis@codeweavers.com>
2018-12-21 13:52:19 -0700

    Add support for uploading 3D S3TC-compressed images.
    This allows clients to initialize 3D image objects with data in a BC1,
    BC2, or BC3 (aka DXTn, aka S3TC) format. Since Metal doesn't support
    this natively, DXTn-compressed image data are manually decompressed
    prior to upload or copying. This particular algorithm was derived from
    Wine, from code originally authored by Connor McAdams. It's somewhat
    primitive, and could probably stand some vectorization, but it's a good
    first start.
    
    The meat of the algorithm is pulled into a header, "MVKDXTnCodec.def",
    intended to be used by both CPU and shader code. CPU uploads, which in
    MoltenVK happen when binding a `VkDeviceMemory` object to an image, run
    the code normally. GPU uploads, i.e. copying from a buffer to a texture,
    run the algorithm as a compute shader, possibly storing the results to
    an intermediate buffer to then copy directly to the texture.
    Intermediate buffers are used when uploading to mip levels higher than
    0, since desktop Metal doesn't support directly writing to higher mip
    levels from a shader.
    
    Only uploads are supported. Downloads are not implemented yet.
    
    Theoretically, this could be extended to other compressed formats as
    well, allowing MoltenVK to support all texture compression formats on
    all platforms.

---
+++ MoltenVK/MoltenVK/Utility/MVKCodec.h
@@ -0,0 +1,50 @@
+/*
+ * MVKCodec.h
+ *
+ * Copyright (c) 2018 Chip Davis for CodeWeavers
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+
+#pragma once
+
+#include "MVKFoundation.h"
+
+#include <string>
+
+
+#pragma mark Texture data codecs
+
+/**
+ * This is the base class implemented by all codecs supported by MoltenVK.
+ * Objects of this class are used to decompress texture data for upload to a
+ * 3D texture.
+ */
+class MVKCodec {
+
+public:
+
+	/** Decompresses compressed texture data for upload. */
+	virtual void decompress(void* pDest, const void* pSrc, const VkSubresourceLayout& destLayout, const VkSubresourceLayout& srcLayout, VkExtent3D extent) = 0;
+
+	/** Destructor. */
+	virtual ~MVKCodec() = default;
+
+};
+
+/** Returns an appropriate codec for the given format, or nullptr if the format is not supported. */
+std::unique_ptr<MVKCodec> mvkCreateCodec(VkFormat format);
+
+/** Returns whether or not the given format can be decompressed. */
+bool mvkCanDecodeFormat(VkFormat format);
