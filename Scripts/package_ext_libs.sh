64648d19c370e9b395224aef5fd7bc45f2166c2d
Bill Hollings <bill.hollings@brenwill.com>
2019-02-24 16:21:20 -0500

    Move MoltenVK clean on dependency build from fetchDependencies.sh to package_ext_libs.sh.

--- Scripts/package_ext_libs.sh
+++ Scripts/package_ext_libs.sh
@@ -9,3 +9,7 @@ rm -rf "${MVK_EXT_LIB_DST_OS_PATH}"
 mkdir -p "${MVK_EXT_LIB_DST_OS_PATH}"
 
 cp -a "${MVK_BUILT_PROD_PATH}/"*.a "${MVK_EXT_LIB_DST_OS_PATH}"
+
+# Clean MoltenVK to ensure the next MoltenVK build will use the latest external library versions.
+make --quiet clean
+
