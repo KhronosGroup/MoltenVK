34047f83f98cc89c817cbc0ecad7238dedd381c7
Chip Davis <cdavis@codeweavers.com>
2019-02-25 14:17:35 -0600

    Only compile in tessellation support kernels on Metal 1.2+.
    And set the stage-in indirect params on 2.1+. I missed this when I added
    setting the stage-in region.

--- MoltenVK/MoltenVK/Commands/MVKCommandPipelineStateFactoryShaderSource.h
+++ MoltenVK/MoltenVK/Commands/MVKCommandPipelineStateFactoryShaderSource.h
@@ -176,6 +176,7 @@ struct MTLStageInRegionIndirectArguments {                                      
 };                                                                                                              \n\
 #endif                                                                                                          \n\
                                                                                                                 \n\
+#if __METAL_VERSION >= 120                                                                                      \n\
 kernel void cmdDrawIndirectConvertBuffers(const device char* srcBuff [[buffer(0)]],                             \n\
                                           device char* destBuff [[buffer(1)]],                                  \n\
                                           constant uint32_t& srcStride [[buffer(2)]],                           \n\
@@ -213,6 +214,10 @@ kernel void cmdDrawIndexedIndirectConvertBuffers(const device char* srcBuff [[bu
     if (idx >= drawCount) { return; }                                                                           \n\
     const device auto& src = *reinterpret_cast<const device MTLDrawIndexedPrimitivesIndirectArguments*>(srcBuff + idx * srcStride);\n\
     device char* dest = destBuff + idx * (sizeof(MTLDispatchThreadgroupsIndirectArguments) + sizeof(MTLDrawPatchIndirectArguments));\n\
+#if __METAL_VERSION__ >= 210                                                                                    \n\
+    device auto& destSI = *(device MTLStageInRegionIndirectArguments*)dest;                                     \n\
+    dest += sizeof(MTLStageInRegionIndirectArguments);                                                          \n\
+#endif                                                                                                          \n\
     device auto& destTC = *(device MTLDispatchThreadgroupsIndirectArguments*)dest;                              \n\
     device auto& destTE = *(device MTLDrawPatchIndirectArguments*)(dest + sizeof(MTLDispatchThreadgroupsIndirectArguments));\n\
     destTC.threadgroupsPerGrid[0] = (src.indexCount * src.instanceCount + controlPointCount - 1) / controlPointCount;\n\
@@ -220,6 +225,11 @@ kernel void cmdDrawIndexedIndirectConvertBuffers(const device char* srcBuff [[bu
     destTE.patchCount = destTC.threadgroupsPerGrid[0];                                                          \n\
     destTE.instanceCount = 1;                                                                                   \n\
     destTE.patchStart = destTE.baseInstance = 0;                                                                \n\
+#if __METAL_VERSION__ >= 210                                                                                    \n\
+    destSI.stageInOrigin[0] = destSI.stageInOrigin[1] = destSI.stageInOrigin[2] = 0;                            \n\
+    destSI.stageInSize[0] = src.instanceCount * max(src.vertexCount, outControlPointCount * destTE.patchCount); \n\
+    destSI.stageInSize[1] = destSI.stageInSize[2] = 1;                                                          \n\
+#endif                                                                                                          \n\
 }                                                                                                               \n\
                                                                                                                 \n\
 kernel void cmdDrawIndexedCopyIndex16Buffer(const device uint16_t* srcBuff [[buffer(0)]],                       \n\
@@ -259,6 +269,8 @@ kernel void cmdDrawIndexedCopyIndex32Buffer(const device uint32_t* srcBuff [[buf
         }                                                                                                       \n\
     }                                                                                                           \n\
 }                                                                                                               \n\
+                                                                                                                \n\
+#endif                                                                                                          \n\
                                                                                                                 \n\
 ";
 
