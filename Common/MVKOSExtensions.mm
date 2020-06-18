bdff52283d68d1051f8c3c73475734b3f69c02d7
Aman Gupta <aman@tmm1.net>
2020-06-17 17:14:31 -0700

    remove !MVK_TVOS usage

--- Common/MVKOSExtensions.mm
+++ Common/MVKOSExtensions.mm
@@ -97,14 +97,16 @@ bool mvkGetEnvVarBool(std::string varName, bool* pWasFound) {
 #pragma mark System memory
 
 uint64_t mvkGetSystemMemorySize() {
-#if !MVK_TVOS
+#if MVK_MACOS || MVK_IOS
 	mach_msg_type_number_t host_size = HOST_BASIC_INFO_COUNT;
 	host_basic_info_data_t info;
 	if (host_info(mach_host_self(), HOST_BASIC_INFO, (host_info_t)&info, &host_size) == KERN_SUCCESS) {
 		return info.max_mem;
 	}
 #endif
+#if MVK_TVOS
 	return 0;
+#endif
 }
 
 uint64_t mvkGetAvailableMemorySize() {
