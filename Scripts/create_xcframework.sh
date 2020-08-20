7579cf6a3b4ebbb62ff20328a8fd03f1187ac157
Bill Hollings <bill.hollings@brenwill.com>
2020-08-20 12:45:12 -0400

    Create external libraries as XCFrameworks.
    Add script copy_to_staging.sh.
    Add script copy_ext_lib_to_staging.sh.
    Add script create_xcframework.sh.
    Add script create_ext_lib_xcframeworks.sh.

---
+++ Scripts/create_xcframework.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# Creates a universal XCFramework for a product from any libraries that can be found for the product.
+#
+# Takes one parameter:
+#   1 - product_name
+#
+# Requires the variables MVK_XCFWK_STAGING_DIR and MVK_XCFWK_DEST_DIR.
+#
+function create_xcframework() {
+	prod_name=${1}
+	xcfwk_path="${MVK_XCFWK_DEST_DIR}/${CONFIGURATION}/${prod_name}.xcframework"
+	hdr_path="${MVK_XCFWK_STAGING_DIR}/Headers/${prod_name}"
+
+	xcfwk_cmd="xcodebuild -create-xcframework -output \"${xcfwk_path}\""
+
+	for prod_staging_dir in "${MVK_XCFWK_STAGING_DIR}/${CONFIGURATION}"/*; do
+		prod_lib_path="${prod_staging_dir}/lib${prod_name}.a"
+		xcfwk_cmd+=" -library \"${prod_lib_path}\""
+		xcfwk_cmd+=" -headers \"${hdr_path}\""
+	done
+
+	rm -rf "${xcfwk_path}"
+	eval "${xcfwk_cmd}"
+}
