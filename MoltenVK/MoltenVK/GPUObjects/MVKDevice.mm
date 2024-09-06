2a6204115dc933eda9251c75c88b1a0a6fe799c7
Nathaniel Cesario <nathaniel@lunarg.com>
2024-09-05 23:00:24 -0600

    Disable sampler2DViewOf3D
    Sets VkPhysicalDeviceImage2DViewOf3DFeaturesEXT::sampler2DViewOf3D to
    false, as CTS tests involving 2D views of sampled 3D textures are not
    currently working.

--- MoltenVK/MoltenVK/GPUObjects/MVKDevice.mm
+++ MoltenVK/MoltenVK/GPUObjects/MVKDevice.mm
@@ -586,7 +586,10 @@ void MVKPhysicalDevice::getFeatures(VkPhysicalDeviceFeatures2* features) {
                 if (getMVKConfig().enable2DViewOf3D) {
                     auto* extFeatures = (VkPhysicalDeviceImage2DViewOf3DFeaturesEXT*)next;
                     extFeatures->image2DViewOf3D = true;
-                    extFeatures->sampler2DViewOf3D = true;
+
+                    // TODO (ncesario-lunarg) Related CTS tests (e.g., dEQP-VK.pipeline.monolithic.image_2d_view_3d_image.compute.sampler.mip0_layer0)
+                    // are failing with this enabled.
+                    extFeatures->sampler2DViewOf3D = false;
                 }
 				break;
 			}
