8506d8f5cd5ca48327f4638a86fd5c482e4fd9db
Evan Tang <etang@codeweavers.com>
2025-02-21 16:46:42 -0600

    Make some MVKDeviceTracking methods const

--- MoltenVK/MoltenVK/GPUObjects/MVKPipeline.mm
+++ MoltenVK/MoltenVK/GPUObjects/MVKPipeline.mm
@@ -120,7 +120,7 @@ std::string MVKPipelineLayout::getLogDescription(std::string indent) {
 	return descStr.str();
 }
 
-bool MVKPipelineLayout::isUsingMetalArgumentBuffers() {
+bool MVKPipelineLayout::isUsingMetalArgumentBuffers() const {
 	return MVKDeviceTrackingMixin::isUsingMetalArgumentBuffers() && _canUseMetalArgumentBuffers;
 }
 
