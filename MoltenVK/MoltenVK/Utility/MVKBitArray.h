76b39f77aa41fadad4543a1c720ed34f059a8339
Evan Tang <etang@codeweavers.com>
2025-04-08 11:27:04 -0500

    Simplify MVKBitArray
    We no longer need to track the highest unset / lowest set bits

--- MoltenVK/MoltenVK/Utility/MVKBitArray.h
+++ MoltenVK/MoltenVK/Utility/MVKBitArray.h
@@ -24,247 +24,40 @@
 #include <type_traits>
 #include <functional>
 
-#pragma mark - MVKBitArray
+namespace detail {
+	static constexpr std::size_t SizeTBits = sizeof(std::size_t) * CHAR_BIT;
 
-/** Represents an array of bits, optimized for reduced storage and fast scanning for bits that are set. */
-class MVKBitArray {
-
-public:
-
-	/**
-	 * Returns the value of the bit, and optionally disables that bit if it was enabled.
-	 * Returns false if the bitIndex is beyond the size of this array.
-	 */
-	bool getBit(size_t bitIndex, bool shouldDisable = false) {
-		if (bitIndex >= _bitCount) { return false; }
-		bool val = mvkIsAnyFlagEnabled(getSection(getIndexOfSection(bitIndex)), getBitPositionSectionMask(bitIndex));
-		if (val && shouldDisable) { disableBit(bitIndex); }
-		return val;
+	template <typename T>
+	static constexpr T maskLo(std::size_t begin) {
+		constexpr std::size_t bits = sizeof(T) * CHAR_BIT;
+		return static_cast<T>(~static_cast<T>(0)) << (begin % bits);
 	}
 
-	/** Sets the value of the bit to the val. */
-	void setBit(size_t bitIndex, bool val) {
-		if (bitIndex >= _bitCount) { return; }
+	template <typename T>
+	static constexpr T maskHi(std::size_t end) {
+		constexpr std::size_t bits = sizeof(T) * CHAR_BIT;
+		return static_cast<T>(~static_cast<T>(0)) >> ((bits - end) % bits);
+	}
 
-		auto secIdx = getIndexOfSection(bitIndex);
-		auto& sectionData = getSection(secIdx);
-		if (val) {
-			mvkEnableFlags(getSection(secIdx), getBitPositionSectionMask(bitIndex));
+	template <typename Fn>
+	static constexpr void applyToBitRange(MVKArrayRef<std::size_t> bits, std::size_t begin, std::size_t end, Fn&& fn) {
+		if (begin >= end) [[unlikely]]
+			return;
+		std::size_t lo = begin / SizeTBits;
+		std::size_t hi = (end - 1) / SizeTBits;
+
+		std::size_t masklo = maskLo<std::size_t>(begin);
+		std::size_t maskhi = maskHi<std::size_t>(end);
+		if (lo == hi) {
+			fn(bits[lo], masklo & maskhi);
 		} else {
-			mvkDisableFlags(getSection(secIdx), getBitPositionSectionMask(bitIndex));
-		}
-
-		// Adjust fully disabled tracker
-		if (isFullyDisabled(sectionData)) {
-			if (secIdx == _fullyDisabledSectionCount) {
-				auto secCnt = getSectionCount();
-				while (++_fullyDisabledSectionCount < secCnt && isFullyDisabled(getSection(_fullyDisabledSectionCount)));
+			fn(bits[lo], masklo);
+			fn(bits[hi], maskhi);
+			for (std::size_t i = lo + 1; i < hi; i++) {
+				fn(bits[i], ~static_cast<std::size_t>(0));
 			}
-		} else {
-			_fullyDisabledSectionCount = std::min(_fullyDisabledSectionCount, (uint32_t)secIdx);
-		}
-
-		// Adjust partially disabled tracker
-		if (isFullyEnabled(sectionData)) {
-			if (secIdx + 1 == _partiallyDisabledSectionCount) {
-				while (--_partiallyDisabledSectionCount > 0 && isFullyEnabled(getSection(_partiallyDisabledSectionCount - 1)));
-			}
-		} else {
-			_partiallyDisabledSectionCount = std::max(_partiallyDisabledSectionCount, (uint32_t)secIdx + 1);
 		}
 	}
-
-	/** Enables the bit. */
-	void enableBit(size_t bitIndex) { setBit(bitIndex, true); }
-
-	/** Enables all bits in the array. */
-	void enableAllBits() {
-		for (size_t secIdx = 0; secIdx < _partiallyDisabledSectionCount; secIdx++) {
-			getSection(secIdx) = FullyEnabledSectionMask;
-		}
-		_partiallyDisabledSectionCount = 0;
-		_fullyDisabledSectionCount = 0;
-	}
-
-	/** Disables the bit. */
-	void disableBit(size_t bitIndex) { setBit(bitIndex, false); }
-
-	/** Disables all bits in the array. */
-	void disableAllBits() {
-		size_t secCnt = getSectionCount();
-		for (size_t secIdx = _fullyDisabledSectionCount; secIdx < secCnt; secIdx++) {
-			getSection(secIdx) = 0;
-		}
-		_partiallyDisabledSectionCount = (uint32_t)secCnt;
-		_fullyDisabledSectionCount = (uint32_t)secCnt;
-	}
-
-	/**
-	 * Returns the index of the first enabled bit, at or after the specified index.
-	 * If no bits are enabled, returns the size() of this bit array.
-	 */
-	size_t getIndexOfFirstEnabledBit(size_t startIndex = 0) {
-		size_t secCnt = getSectionCount();
-		size_t secIdx = getIndexOfSection(startIndex);
-
-		// Optimize by skipping all consecutive sections at the beginning that are known to have no enabled bits.
-		if (secIdx < _fullyDisabledSectionCount) {
-			secIdx = _fullyDisabledSectionCount;
-			startIndex = 0;
-		}
-
-		// Search all sections at or after the starting index, and if an enabled bit is found, return the index of it.
-		while (secIdx < secCnt) {
-			size_t lclBitIdx = getIndexOfFirstEnabledBitInSection(getSection(secIdx), getBitIndexInSection(startIndex));
-			if (lclBitIdx < SectionBitCount) {
-				return (secIdx * SectionBitCount) + lclBitIdx;
-			}
-			startIndex = 0;
-			secIdx++;
-		}
-
-		return _bitCount;
-	}
-
-	/**
-	 * Enumerates the bits, executing a custom function on each bit that is enabled.
-	 *
-	 * The function to execute is passed a bitIndex parameter which indicates
-	 * the index of the bit for which the function is executing.
-	 *
-	 * The custom function should return true to continue processing further bits, or false
-	 * to stop processing further bits. This function returns false if any of the invocations
-	 * of the custom function halted further invocations, and returns true otherwise.
-	 */
-	bool enumerateEnabledBits(std::function<bool(size_t bitIndex)> func) {
-		for (size_t bitIdx = getIndexOfFirstEnabledBit();
-			 bitIdx < _bitCount;
-			 bitIdx = getIndexOfFirstEnabledBit(++bitIdx)) {
-
-			if ( !func(bitIdx) ) { return false; }
-		}
-		return true;
-	}
-
-	/** Returns the number of bits in this array. */
-	size_t size() const { return _bitCount; }
-
-	/** Returns whether this array is empty. */
-	bool empty() const { return _bitCount == 0; }
-
-	/**
-	 * Resize this array to the specified number of bits.
-	 *
-	 * The value of existing bits that fit within the new size are retained, and any
-	 * new bits that are added to accommodate the new size are set to the given value.
-	 *
-	 * If the new size is larger than the existing size, new memory is allocated.
-	 * If the new size is less than the existing size, consumed memory is retained.
-	 */
-	void resize(size_t size, bool val = false) {
-		assert(size < SectionBitCount * std::numeric_limits<uint32_t>::max());	// Limited by _partially/fullyDisabledSectionCount
-
-		if (size == _bitCount) { return; }
-
-		size_t oldBitCnt = _bitCount;
-		size_t oldSecCnt = getSectionCount();
-		size_t oldEndBitCnt = oldSecCnt * SectionBitCount;
-
-		_bitCount = size;	// After here, functions refer to new data characteristics.
-
-		// If the number of data sections is not growing, we retain the existing data memory,
-		// to avoid having to reallocate if this array is resized larger in the future.
-		// If the number of data sections is growing, we need to expand memory.
-		size_t newSecCnt = getSectionCount();
-		if (newSecCnt == oldSecCnt) {
-			// The number of data sections is staying the same.
-			// Keep the existing data, but fill any bits in the last section
-			// that were beyond the old bit count with the new initial value.
-			for (size_t bitIdx = oldBitCnt; bitIdx < _bitCount; bitIdx++) { setBit(bitIdx, val); }
-		} else if (newSecCnt > oldSecCnt) {
-			// The number of data sections is growing.
-			// Reallocate new memory to keep the existing contents.
-			_data = (uint64_t*)realloc(_data, newSecCnt * SectionByteCount);
-
-			// Fill any bits in the last section that were beyond the old bit count with the fill value.
-			for (size_t bitIdx = oldBitCnt; bitIdx < oldEndBitCnt; bitIdx++) { setBit(bitIdx, val); }
-
-			// Fill the additional sections with the fill value.
-			uint64_t* pExtraData = &_data[oldSecCnt];
-			memset(pExtraData, val ? (uint8_t)FullyEnabledSectionMask : 0, (newSecCnt - oldSecCnt) * SectionByteCount);
-
-			// If the additional sections have been cleared, extend the associated trackers.
-			if ( !val ) {
-				if (_partiallyDisabledSectionCount == oldSecCnt) { _partiallyDisabledSectionCount = (uint32_t)newSecCnt; }
-				if (_fullyDisabledSectionCount == oldSecCnt) { _fullyDisabledSectionCount = (uint32_t)newSecCnt; }
-			}
-		} else {
-			// The number of data sections is shrinking.
-			// Retain existing allocation, but ensure these values still fit.
-			_partiallyDisabledSectionCount = std::min(_partiallyDisabledSectionCount, (uint32_t)newSecCnt);
-			_fullyDisabledSectionCount = std::min(_fullyDisabledSectionCount, (uint32_t)newSecCnt);
-		}
-	}
-
-	/** Resets back to zero size and frees all data. */
-	void reset() {
-		free(_data);
-		_data = nullptr;
-		_bitCount = 0;
-		_partiallyDisabledSectionCount = 0;
-		_fullyDisabledSectionCount = 0;
-	}
-
-	/** Constructs an instance for the specified number of bits, and sets the initial value of all the bits. */
-	MVKBitArray(size_t size = 0, bool val = false) { resize(size, val); }
-
-	MVKBitArray(const MVKBitArray& other) {
-		resize(other._bitCount);
-		memcpy(_data, other._data, getSectionCount() * SectionByteCount);
-	}
-
-	MVKBitArray& operator=(const MVKBitArray& other) {
-		resize(other._bitCount);
-		memcpy(_data, other._data, getSectionCount() * SectionByteCount);
-		return *this;
-	}
-
-	~MVKBitArray() { reset(); }
-
-protected:
-
-	uint64_t& getSection(size_t secIdx) { return _data[secIdx]; }
-	size_t getSectionCount() const { return _bitCount ? getIndexOfSection(_bitCount - 1) + 1 : 0; }
-
-	static size_t getIndexOfSection(size_t bitIndex) { return bitIndex / SectionBitCount; }
-	static uint8_t getBitIndexInSection(size_t bitIndex) { return bitIndex & (SectionBitCount - 1); }
-	static bool isFullyEnabled(uint64_t sectionData) { return sectionData == FullyEnabledSectionMask; }
-	static bool isFullyDisabled(uint64_t sectionData) { return sectionData == 0; }
-
-	// Returns a section mask containing a single 1 value in the bit in the section that
-	// corresponds to the specified global bit index, and 0 values in all other bits.
-	static uint64_t getBitPositionSectionMask(size_t bitIndex) {
-		return (uint64_t)1U << ((SectionBitCount - 1) - getBitIndexInSection(bitIndex));
-	}
-
-	// Returns the local index of the first enabled bit in the section, starting from the highest order bit.
-	// Disables all bits ahead of the start bit so they will be ignored, then counts the number of zeros
-	// ahead of the set bit. If there are no enabled bits, returns the number of bits in a section.
-	static uint8_t getIndexOfFirstEnabledBitInSection(uint64_t section, uint8_t lclStartBitIndex) {
-		uint64_t lclStartMask = FullyEnabledSectionMask;
-		lclStartMask >>= lclStartBitIndex;
-		section &= lclStartMask;
-		return section ? __builtin_clzll(section) : SectionBitCount;
-	}
-
-	static constexpr size_t SectionBitCount = 64;
-	static constexpr size_t SectionByteCount = SectionBitCount / 8;
-	static constexpr uint64_t FullyEnabledSectionMask = ~static_cast<uint64_t>(0);
-
-	uint64_t* _data = nullptr;
-	size_t _bitCount = 0;
-	uint32_t _partiallyDisabledSectionCount = 0;	// Tracks where to stop filling when enabling all bits
-	uint32_t _fullyDisabledSectionCount = 0;		// Tracks where to start looking for enabled bits
 };
 
 #pragma mark - MVKStaticBitArray
@@ -417,28 +210,9 @@ public:
 
 template <uint32_t Bits>
 class MVKLargeStaticBitSet {
-	static constexpr std::size_t ElemSize = 8 * sizeof(std::size_t);
+	static constexpr std::size_t ElemSize = detail::SizeTBits;
 	static constexpr std::size_t ArrayLen = (Bits + ElemSize - 1) / ElemSize;
 	std::size_t bits[ArrayLen];
-
-	template <typename Fn>
-	constexpr void applyToRange(std::size_t begin, std::size_t end, Fn&& fn) {
-		if (begin >= end) [[unlikely]]
-			return;
-		std::size_t lo = begin / ElemSize;
-		std::size_t hi = (end - 1) / ElemSize;
-		std::size_t masklo = ~static_cast<std::size_t>(0) << (begin % ElemSize);
-		std::size_t maskhi = ~static_cast<std::size_t>(0) >> ((ElemSize - end) % ElemSize);
-		if (lo == hi) {
-			fn(bits[lo], masklo & maskhi);
-		} else {
-			fn(bits[lo], masklo);
-			fn(bits[hi], maskhi);
-			for (std::size_t i = lo + 1; i < hi; i++) {
-				fn(bits[i], ~static_cast<std::size_t>(0));
-			}
-		}
-	}
 
 public:
 	/** Construct an empty bitset */
@@ -467,12 +241,12 @@ public:
 
 	/** Set all bits in the given range */
 	constexpr void setRange(std::size_t begin, std::size_t end) {
-		applyToRange(begin, end, [](std::size_t& val, std::size_t mask){ val |= mask; });
+		detail::applyToBitRange(bits, begin, end, [](std::size_t& val, std::size_t mask){ val |= mask; });
 	}
 
 	/** Clear all bits in the given range */
 	constexpr void clearRange(std::size_t begin, std::size_t end) {
-		applyToRange(begin, end, [](std::size_t& val, std::size_t mask){ val &= ~mask; });
+		detail::applyToBitRange(bits, begin, end, [](std::size_t& val, std::size_t mask){ val &= ~mask; });
 	}
 
 	/** Set or clear the given bit */
@@ -557,3 +331,190 @@ public:
  */
 template <uint32_t Bits>
 using MVKStaticBitSet = std::conditional_t<(Bits > 32), MVKLargeStaticBitSet<Bits>, MVKSmallStaticBitSet<std::conditional_t<(Bits > 16), uint32_t, uint16_t>>>;
+
+#pragma mark - MVKBitArray
+
+class MVKBitArray {
+	static constexpr std::size_t ElemSize = detail::SizeTBits;
+	std::size_t* _data = nullptr;
+	std::size_t _size = 0;
+	// Small array optimization: For small arrays, store data in _capacity
+	std::size_t _capacity = 0;
+
+	static std::size_t elemCount(size_t size) {
+		return (size + ElemSize - 1) / ElemSize;
+	}
+
+	std::size_t capacity() const {
+		return _data == &_capacity ? ElemSize : _capacity;
+	}
+
+	void freeBuffer() {
+		if (_data != &_capacity)
+			free(_data);
+	}
+
+	MVKArrayRef<std::size_t> elems() {
+		return { _data, elemCount(_size) };
+	}
+
+	MVKArrayRef<const std::size_t> elems() const {
+		return { _data, elemCount(_size) };
+	}
+
+public:
+	MVKBitArray(): _data(&_capacity) {}
+	MVKBitArray(std::size_t size, bool value): MVKBitArray() { resizeAndClear(size, value); }
+	MVKBitArray(const MVKBitArray& other): MVKBitArray() {
+		_size = other._size;
+		if (other._size <= ElemSize) {
+			*_data = *other._data;
+		} else {
+			size_t elems = elemCount(other._size);
+			size_t bytes = elems * sizeof(std::size_t);
+			_capacity = elems * ElemSize;
+			_data = static_cast<std::size_t*>(malloc(bytes));
+			memcpy(_data, other._data, bytes);
+		}
+	}
+	~MVKBitArray() { freeBuffer(); }
+
+	MVKBitArray& operator=(const MVKBitArray& other) {
+		_size = other._size;
+		size_t elems = elemCount(other._size);
+		if (other._size <= ElemSize) {
+			*_data = *other._data;
+		} else {
+			size_t bytes = elems * sizeof(std::size_t);
+			if (other._size > capacity()) {
+				freeBuffer();
+				_data = static_cast<std::size_t*>(malloc(bytes));
+				_capacity = elems * ElemSize;
+			}
+			memcpy(_data, other._data, bytes);
+		}
+		return *this;
+	}
+
+	/**
+	 * Resize this array to the specified number of bits, and reset all bits to the given value.
+	 *
+	 * This is faster than separately resizing and clearing the bits.
+	 *
+	 * If the new size is larger than the existing size, new memory may be allocated.
+	 * If the new size is less than the existing size, consumed memory is retained.
+	 */
+	void resizeAndClear(std::size_t newSize, bool value = false) {
+		size_t elems = elemCount(newSize);
+		_size = newSize;
+
+		if (newSize <= ElemSize) {
+			*_data = value ? MVKSmallStaticBitSet<std::size_t>::range(0, newSize).bits() : 0;
+			return;
+		}
+
+		if (newSize > capacity()) {
+			freeBuffer();
+			_data = static_cast<std::size_t*>(calloc(elems, sizeof(std::size_t)));
+			_capacity = elems * ElemSize;
+			if (value)
+				memset(_data, 0xff, elems * sizeof(std::size_t));
+		} else {
+			memset(_data, value ? 0xff : 0, elems * sizeof(std::size_t));
+		}
+		if (value)
+			_data[elems - 1] = detail::maskHi<std::size_t>(newSize);
+	}
+
+	/**
+	 * Resize this array to the specified number of bits.
+	 *
+	 * The value of existing bits that fit within the new size are retained, and any
+	 * new bits that are added to accommodate the new size are set to the given value.
+	 *
+	 * If the new size is larger than the existing size, new memory may be allocated.
+	 * If the new size is less than the existing size, consumed memory is retained.
+	 */
+	void resize(std::size_t newSize, bool value = false) {
+		size_t elems = elemCount(newSize);
+		size_t oldSize = _size;
+		_size = newSize;
+		if (newSize <= oldSize) {
+			if (newSize)
+				_data[elems - 1] &= detail::maskHi<std::size_t>(newSize);
+		} else {
+			size_t bytes = elems * sizeof(std::size_t);
+			if (newSize > capacity()) {
+				if (_data == &_capacity) {
+					_data = static_cast<std::size_t*>(malloc(bytes));
+					*_data = _capacity;
+				} else {
+					_data = static_cast<std::size_t*>(realloc(_data, bytes));
+				}
+				_capacity = elems * ElemSize;
+				// The rest of the elements will get overwritten by the set/clear range
+				_data[elems - 1] = 0;
+			}
+			if (value)
+				setRange(oldSize, newSize);
+			else
+				clearRange(oldSize, newSize);
+		}
+	}
+
+	/** Get an iterator over the set bits in this array */
+	MVKSetBitIteratorIterator<std::size_t> setBitsList() const { return { _data, 0, elemCount(_size) }; }
+
+	/** Set all bits in the given range */
+	void setRange(std::size_t begin, std::size_t end) {
+		assert(end <= _size);
+		detail::applyToBitRange(elems(), begin, end, [](std::size_t& val, std::size_t mask){ val |= mask; });
+	}
+
+	/** Clear all bits in the given range */
+	void clearRange(std::size_t begin, std::size_t end) {
+		assert(end <= _size);
+		detail::applyToBitRange(elems(), begin, end, [](std::size_t& val, std::size_t mask){ val &= ~mask; });
+	}
+
+	/** Set or clear the given bit */
+	void set(MVKBitPointer<std::size_t> bit, bool value = true) {
+		assert(static_cast<std::size_t>(bit) < _size);
+		std::size_t& word = _data[bit.wordOffset];
+		std::size_t flag = static_cast<std::size_t>(1ull << bit.bitOffset);
+		word = value ? word | flag : word & ~flag;
+	}
+	/** Set or clear the given bit */
+	void set(std::size_t bit, bool value = true) { set({ bit / ElemSize, bit % ElemSize }, value); }
+	/** Convenience function for set(bit, false) */
+	void clear(MVKBitPointer<std::size_t> bit) { set(bit, false); }
+	/** Convenience function for set(bit, false) */
+	void clear(std::size_t bit) { set(bit, false); }
+	/** Check if the given bit is set */
+	bool get(MVKBitPointer<std::size_t> bit) const {
+		assert(static_cast<std::size_t>(bit) < _size);
+		return (_data[bit.wordOffset] >> bit.bitOffset) & 1;
+	}
+	/** Check if the given bit is set */
+	bool get(std::size_t bit) const { return get({ bit / ElemSize, bit % ElemSize }); }
+	/** Check if any bits in the array are set */
+	bool areAnyBitsSet() const {
+		for (std::size_t elem : elems())
+			if (elem)
+				return false;
+		return true;
+	}
+	/** Check if all bits in the array are unset */
+	bool areAllBitsClear() const { return !areAnyBitsSet(); }
+
+	bool operator==(const MVKBitArray& other) const {
+		if (_size != other._size)
+			return false;
+		size_t end = elemCount(_size);
+		for (size_t i = 0; i < end; i++)
+			if (_data[i] != other._data[i])
+				return false;
+		return true;
+	}
+	bool operator!=(const MVKBitArray& other) const { return !(*this == other); }
+};
