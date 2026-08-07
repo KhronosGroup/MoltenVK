31196c77181786934e3018cd5e44ed36bbdf9e53
David Grinberg <green.grinya@gmail.com>
2026-08-08 00:12:39 +0700

    oops

--- MoltenVK/MoltenVK/GPUObjects/MVKQueryPool.h
+++ MoltenVK/MoltenVK/GPUObjects/MVKQueryPool.h
@@ -92,7 +92,7 @@ public:
     /** Called from the MVKCmdBeginQuery command when it is added to the command buffer */
     virtual void beginQueryAddedTo(uint32_t query, MVKCommandBuffer* cmdBuffer) {};
 
-    /**  Returns whether queryCount queries starting at firstQuery are available on the device. */
+    /** Returns whether queryCount queries starting at firstQuery are available on the device. */
     bool areQueriesDeviceAvailable(uint32_t firstQuery, uint32_t queryCount);
 
 #pragma mark Construction
