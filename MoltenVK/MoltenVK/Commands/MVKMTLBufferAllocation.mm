1af529ceaa28812e932317f71df2e88c83f58ded
kisasexypantera94 <green.grinya@gmail.com>
2026-09-20 05:12:22 +0400

    cleanup

--- MoltenVK/MoltenVK/Commands/MVKMTLBufferAllocation.mm
+++ MoltenVK/MoltenVK/Commands/MVKMTLBufferAllocation.mm
@@ -6,9 +6,9 @@
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *     http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -56,8 +56,6 @@ MVKMTLBufferAllocation* MVKMTLBufferAllocationPool::acquireAllocationUnlocked() 
         [ba->_mtlBuffer setPurgeableState: MTLPurgeableStateNonVolatile];
     }
 
-    // Keep the pool alive until this allocation is returned by the
-    // MTLCommandBuffer completion handler.
     retain();
     return ba;
 }
@@ -88,8 +86,6 @@ void MVKMTLBufferAllocationPool::returnAllocation(MVKMTLBufferAllocation* ba) {
         returnAllocationUnlocked(ba);
     }
 
-    // The final release may destroy the pool, so do this after
-    // releasing the pool mutex.
     release();
 }
 
